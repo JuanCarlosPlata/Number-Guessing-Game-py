c80f606693b7c669285d321a6096d33bb6522a5e
JuanCarlosPlata <platapradajuancarlos@gmail.com>
2023-05-18 20:26:15 +0100

    terminado

--- main.py
+++ main.py
@@ -16,30 +16,30 @@ def validar_numero(text):
             delay_print(text)
             valor = int(input("\n=> "))
             if valor <1 or valor >20:
-                 delay_print(f"Invalid option\nPlease only enter numbers betwen 1-20\n")
+                 delay_print(f"\nPor favor solo ingrese números entre 1-20\n")
                  time.sleep(1)
                  os.system("clear")
                  continue
         except ValueError:
-            delay_print(f"Invalid option\nPlease only enter numbers betwen 1-20\n")
+            delay_print(f"\nPor favor solo ingrese números entre 1-20\n")
             time.sleep(1)
             os.system("clear")
             continue
         return valor
 
 
-delay_print("Saludos, vamos a jugar a las adivinanzas\nYo escogere u numero entre 1-20 y tu deberas adivinar cual es")
-randon_number= random.randrange(1,20)
+delay_print("Saludos, vamos a jugar a las adivinanzas\nYo escogeré u numero entre 1-20 y tu deberás adivinar cual es")
+random_number= random.randrange(1,20)
 
 while True:
     numero = validar_numero("\nIngresa un numero entre 1-20")
-    if numero < randon_number:
+    if numero < random_number:
         delay_print(f"El numero a adivinar es mayor a {numero}")
-        time.sleep(2)
+        time.sleep(1.5)
         os.system("clear")
-    elif numero > randon_number:
+    elif numero > random_number:
         delay_print(f"El numero a adivinar es menor a {numero}")
-        time.sleep(2)
+        time.sleep(1.5)
         os.system("clear")
     else:
         delay_print(f"Felicidades! acertaste, el numero era {numero}")
